6708c49d9eb2183bafa4f0555d19e00af5567edb
yAYANx20 <ayanakash936@gmail.com>
2024-10-11 04:29:30 +0000

    Adding new project

---
+++ src/test/java/com/spree/testdata/Spreecom_TestData.java
@@ -0,0 +1,18 @@
+package com.spree.testdata;
+
+import org.testng.annotations.DataProvider;
+
+public class Spreecom_TestData {
+	@DataProvider(name="country_iso")
+	public Object[][] iso_name(){
+		// Two dimensional object
+	    return new Object[][] {
+	            {"usa","UNITED STATES","USA"},
+	            {"ind","INDIA","IND"},
+	            {"pak","PAKISTAN","PAK"},
+	            {"gb","UNITED KINGDOM","GBR"},
+	            {"afg","AFGHANISTAN","AFG"}
+	            };
+	}
+
+}
